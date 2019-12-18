7527660411a0c3c8428e450e5f6f59df01274012
Amber Martus <aamartus8@gmail>
2019-12-18 07:57:54 -0700

    stuff

--- final.js
+++ final.js
@@ -2,3 +2,108 @@ function ready () {
     console.log ("Page Ready");
     document.forms.fav
 }
+// let multipleChoice = document.getElementById('multiplechoice')
+// let numberright = 0
+
+// document.getElementById('goal creation').onclick = function()
+
+const start = document.getElementById("start");
+
+const quiz = document.getElementById("quiz");
+
+const question = document.getElementById("question");
+
+const qImg = document.getElementById("qImg");
+
+const choiceA = document.getElementById("A");
+
+const choiceB = document.getElementById("B");
+
+const choiceC = document.getElementById("C");
+
+const counter = document.getElementById("counter");
+
+const timeGauge = document.getElementById("timeGauge");
+
+const progress = document.getElementById("progress");
+
+const scoreDiv = document.getElementById("scoreContainer");
+
+let questions = [
+
+    {
+
+        question : "Which part of the skeletal system is represented by N?",
+
+        imgSrc : "https://i.pinimg.com/originals/4f/93/4e/4f934ead9d447dcdff81fe77f269b053.jpg",
+
+        choiceA : "Femur",
+
+        choiceB : "Ulna",
+
+        choiceC : "Tibia",
+
+        correct : "A"
+
+    },{
+
+        question : "Which part of the skeletal system is represented by H?",
+
+        imgSrc : "https://i.pinimg.com/originals/4f/93/4e/4f934ead9d447dcdff81fe77f269b053.jpg,
+
+        choiceA : "humerus",
+
+        choiceB : "sacrum",
+
+        choiceC : "clavicle",
+
+        correct : "B"
+
+    },{
+
+        question : "What does JS stand for?",
+
+        imgSrc : "https://i.pinimg.com/originals/4f/93/4e/4f934ead9d447dcdff81fe77f269b053.jpg",
+
+        choiceA : "Wrong",
+
+        choiceB : "Wrong",
+
+        choiceC : "Correct",
+
+        correct : "C"
+
+    }
+
+];
+const lastQuestion = questions.length - 1;
+
+let runningQuestion = 0;
+
+function renderQuestion(){
+
+    let q = questions[runningQuestion];
+
+   
+
+    question.innerHTML = "<p>"+ q.question +"</p>";
+
+    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+
+    choiceA.innerHTML = q.choiceA;
+
+    choiceB.innerHTML = q.choiceB;
+
+    choiceC.innerHTML = q.choiceC;
+
+}
+function renderProgress(){
+
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
+
+        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
+
+    }
+}
+
+start.addEventListener("click",startQuiz);
